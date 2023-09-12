7ffc7a37a199fc32eccfde9509a53a9580f55665
Diêgo de Barros <debarrosdiego415@gmail.com>
2023-09-12 19:43:28 -0300

    Fim da requisição dos pokemons + trazendo lista dos mesmos para o html

--- source/main.js
+++ source/main.js
@@ -1,64 +1,19 @@
-//url pokemons request
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-//html element of the Ol pokemon list
-const pokemonList = document.getElementById('pokemonsOlList');
+function convertPokemonsToLi(pokemon){
+return `<li class="pokemonCardLi">    
+                <div class="cardImgConteiner">
+                    <img class="pokemonCardImg" src=${pokemon.sprites.other.dream_world.front_default} alt="">            
+                </div>
+                <span class="pokemonCardName">${pokemon.name}</span>
+                <span class="pokemonCardNumber">#${pokemon.order}</span>
+            </li>`
+}
 
+const pokemonList = document.getElementById('pokemonsList');
 
-//--convert pokemon JSON list to html -------------
-function createPokemonOlList (pokemonsJson){
+pokeApi.getPokemons().then((pokemons = []) => {
   
-  //detalhar pokemon
-  async function detailPokemon(pokemon){
-    const response = await fetch(pokemon.url)
-    .then(response => response.json())
-    .then(response => {console.log(response); return response;});
-    return response;
-  }
+  const newHtml = pokemons.map(convertPokemonsToLi).join('')
+  // console.log(newHtml)
+  pokemonList.innerHTML += newHtml
+})
 
-
-  // --- ... continuar essa função e a de detail pokemon ?
-  const stringReturn = pokemonsJson.results.map(function(listedPokemon){
-
-
-    const detailedPokemon = detailPokemon(listedPokemon);
-
-    return `<li class="pokemonCardLi">    
-    <div class="cardImgConteiner">
-    <img class="pokemonCardImg" src='' alt="${listedPokemon.name}">            
-    </div>
-    <span class="pokemonCardName">${listedPokemon.name}</span>
-    <span class="pokemonCardNumber">${detailedPokemon.weight}</span>
-    </li>
-    `;
-      }
-    );
- 
-    return stringReturn.join('');
-  }
-
-
-
-
-
-
-
-
-
-
-//fetching the api pokemons list endpoint and adding li's to the html for each pokemon
-fetch(url)
-  .then(response => response.json())
-  .then(responseJson => {console.log(responseJson); return responseJson;})
-  .then(pokemonsJson =>  createPokemonOlList(pokemonsJson))
-  .then(pokemonHtmlList => pokemonList.innerHTML += pokemonHtmlList);
-
-
-
-
-
-
-
-
-
-
-  
